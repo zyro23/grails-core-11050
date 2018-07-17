f7057e3526c6a6408733f5b5dc6f6b271edb59ed
Jeff Scott Brown <brownj@objectcomputing.com>
2018-07-17 16:05:15 -0500

    Troubleshooting failing test

--- src/integration-test/groovy/myapp/foo/FooIntegrationSpec.groovy
+++ src/integration-test/groovy/myapp/foo/FooIntegrationSpec.groovy
@@ -3,7 +3,7 @@ package myapp.foo
 import grails.testing.mixin.integration.Integration
 import spock.lang.Specification
 
-@Integration(applicationClass = TestApplication)
+@Integration(applicationClass = Application)
 class FooIntegrationSpec extends Specification {
 
 	void "test custom integration test application class"() {
